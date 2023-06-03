ec2a9c52aea527254ede5ba58d02779d79f4251f
Youssef AMEACHAQ <youssefameachaq@Youssefs-MacBook-Pro.local>
2023-06-03 01:05:50 +0100

    WIP: working on string extractions (tests)

--- src/strings/index.ts
+++ src/strings/index.ts
@@ -1,4 +1,14 @@
-type extractionType = "braces" | "db_braces" | "bracket" | "parenthesis" | "quote" | "double_quote";
+type extractionTypes = "braces" | "db_braces" | "brackets" | "parenthesis" | "quote" | "double_quote";
+const extractionTypeRegEx : {
+    [k: string]: RegExp
+} = {
+    braces: new RegExp(/\{.*?\}/g),
+    db_braces: new RegExp(/\{\{(.*?)\}\}/g),
+    brackets: new RegExp(/\((.*?)\)/g),
+    parenthesis: new RegExp(/\((.*?)\)/g),
+    quote: new RegExp(/'.*?'/g),
+    double_quote: new RegExp(/\"(.*?)\"/g)
+}
 
 /**
  * 
@@ -6,28 +16,13 @@ type extractionType = "braces" | "db_braces" | "bracket" | "parenthesis" | "quot
  * @param by the type of extraction : braces, bracket, parenthesis, quote or double_quote
  * @returns Array of string or undefined
  */
-export const $extract = (text: string, by: extractionType): string[] | undefined => {
-    const first = getFirstAndLastExtractionChar(by)[0];
-    const last = getFirstAndLastExtractionChar(by)[1];
-    const regex = RegExp("/\\" + first + "(.*?)\\" + last + "/g");
-    console.log("res : ", regex)
-    return text.match(regex)?.map($0 => $0.toString());
-}
-const getFirstAndLastExtractionChar = (by: extractionType): [string, string] => {
-    switch (by) {
-        case "braces":
-            return ["{", "}"];
-        case "db_braces":
-            return ["{{", "}}"];
-        case "bracket":
-            return ["[", "]"];
-        case "parenthesis":
-            return ["(", ")"];
-        case "quote":
-            return ["'", "'"];
-        case "double_quote":
-            return ["\"", "\""];
-        default:
-            return ["\"", "\""];
-    }
+export const $extract = (text: string, by: extractionTypes): string[] => {
+    console.log(extractionTypeRegEx[by])
+    const matchRes = text.match(extractionTypeRegEx[by]);
+    if (!matchRes) return [];
+    return matchRes.map($0 => {
+        const match = $0.toString();
+        const spliceIndex = by.startsWith('db_') ? 2 : 1; 
+        return `${match.slice(spliceIndex, match.length - spliceIndex)}`;
+    });
 }
--- src/strings/test.spec.ts
+++ src/strings/test.spec.ts
@@ -1,15 +1,58 @@
-import { describe, expect, jest, test } from '@jest/globals';
-import { JSDOM } from 'jsdom';
+import { describe, expect, test } from '@jest/globals';
 import { $extract } from './';
 
 // beforeEach(() => {
 // });
-// "braces" | "db_braces" | "bracket" | "parenthesis" | "quote" | "double_quote"
+// "" | "" | "" | "" | "double_quote"
 describe('Test text extractions', () => {
   test('Test braces', async () => {
     const text = "hello {t} from morocco {r} :)";
+    const arr = $extract(text, "braces");
+    expect(arr).not.toEqual(undefined);
+    expect(arr!.length).toEqual(2);
+    expect(arr![0]).toEqual("t");
+    expect(arr![1]).toEqual("r");
+  });
+  
+  test('Test db_braces', async () => {
+    const text = "hello {{t}} from morocco {{r}} :)";
     const arr = $extract(text, "db_braces");
-    console.log(arr)
+    expect(arr).not.toEqual(undefined);
+    expect(arr!.length).toEqual(2);
+    expect(arr![0]).toEqual("t");
+    expect(arr![1]).toEqual("r");
+  });
+  
+  test('Test parenthesis', async () => {
+    const text = "hello (t) from morocco (r) :)";
+    const arr = $extract(text, "parenthesis");
+    expect(arr).not.toEqual(undefined);
+    expect(arr!.length).toEqual(2);
+    expect(arr![0]).toEqual("t");
+    expect(arr![1]).toEqual("r");
+  });
+  
+  test('Test brackets', async () => {
+    const text = "hello [t] from morocco [r] :)";
+    const arr = $extract(text, "brackets");
+    expect(arr).not.toEqual(undefined);
+    expect(arr!.length).toEqual(2);
+    expect(arr![0]).toEqual("t");
+    expect(arr![1]).toEqual("r");
+  });
+  
+  test('Test quote', async () => {
+    const text = "hello 't' from morocco 'r' :)";
+    const arr = $extract(text, "quote");
+    expect(arr).not.toEqual(undefined);
+    expect(arr!.length).toEqual(2);
+    expect(arr![0]).toEqual("t");
+    expect(arr![1]).toEqual("r");
+  });
+  
+  test('Test double quote', async () => {
+    const text = 'hello "t" from morocco "r" :)';
+    const arr = $extract(text, "double_quote");
     expect(arr).not.toEqual(undefined);
     expect(arr!.length).toEqual(2);
     expect(arr![0]).toEqual("t");
